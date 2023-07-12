23f532786537513e37b37ec771227f6ff530e687
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-12 15:19:20 +0200

    Refactor AuthorModel class

--- module-repository/src/main/java/com/mjc/school/repository/model/AuthorModel.java
+++ module-repository/src/main/java/com/mjc/school/repository/model/AuthorModel.java
@@ -1,22 +1,23 @@
 package com.mjc.school.repository.model;
 
-import com.mjc.school.repository.dto.AuthorModelRequest;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "authors")
+@Data
 public class AuthorModel implements BaseEntity<Long> {
-    private static Long idGenerator = 0L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(nullable = false, length = 15, unique = true)
     private String name;
 
@@ -31,62 +32,4 @@ public class AuthorModel implements BaseEntity<Long> {
     @OneToMany(mappedBy = "authorModel", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<NewsModel> news;
 
-    public AuthorModel(AuthorModelRequest authorModelRequest) {
-        idGenerator++;
-        this.setId(AuthorModel.idGenerator);
-        this.setName(authorModelRequest.getName());
-        this.setCreateDate(LocalDateTime.now());
-        this.setLastUpdateTime(LocalDateTime.now());
-    }
-
-    public AuthorModel() {
-        idGenerator++;
-        this.setId(AuthorModel.idGenerator);
-        this.setCreateDate(LocalDateTime.now());
-        this.setLastUpdateTime(LocalDateTime.now());
-    }
-
-    public AuthorModel(String empty) {
-        this.setId(AuthorModel.idGenerator);
-        this.setCreateDate(LocalDateTime.now());
-        this.setLastUpdateTime(LocalDateTime.now());
-    }
-
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDateTime createDate) {
-        this.createDate = createDate;
-    }
-
-    public LocalDateTime getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Long id) {
-        if (id != null) {
-            this.id = id;
-        } else
-            this.id = AuthorModel.idGenerator;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
