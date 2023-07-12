c430621a92dc2c99d929e41f2f923062da08bb36
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-12 15:34:27 +0200

    Refactor TagModel class

--- module-repository/src/main/java/com/mjc/school/repository/model/TagModel.java
+++ module-repository/src/main/java/com/mjc/school/repository/model/TagModel.java
@@ -1,11 +1,14 @@
 package com.mjc.school.repository.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tags")
+@Data
 public class TagModel implements BaseEntity<Long> {
 
     @Id
@@ -17,58 +20,5 @@ public class TagModel implements BaseEntity<Long> {
 
     @ManyToMany(mappedBy = "tagModels", fetch = FetchType.LAZY)
     private List<NewsModel> newsModels;
-
-    public TagModel() {
-    }
-
-    public TagModel(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public TagModel(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<NewsModel> getNewsModels() {
-        return newsModels;
-    }
-
-    public void setNewsModels(List<NewsModel> news) {
-        this.newsModels = news;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TagModel tagModel = (TagModel) o;
-        return id.equals(tagModel.id) &&
-                name.equals(tagModel.name) &&
-                Objects.equals(newsModels, tagModel.newsModels);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, newsModels);
-    }
 
 }
