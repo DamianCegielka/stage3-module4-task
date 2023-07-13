bac73f7f4e05e0fbc300d8ad8d77b4227e28dca4
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-13 17:29:37 +0200

    Add partly implementation for AuthorController

--- module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
@@ -1,44 +1,60 @@
 package com.mjc.school.controller.impl;
 
-import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.annotations.CommandHandler;
+import com.mjc.school.controller.BaseRestController;
 import com.mjc.school.service.AuthorService;
 import com.mjc.school.service.dto.author.AuthorDtoRequest;
 import com.mjc.school.service.dto.author.AuthorDtoResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/authors", consumes = {"application/JSON"}, produces = {"application/JSON"})
 @AllArgsConstructor
-public class AuthorController implements BaseController<AuthorDtoRequest, AuthorDtoResponse, Long>{
+public class AuthorController implements BaseRestController<AuthorDtoRequest, AuthorDtoResponse, Long> {
 
-  private final AuthorService service;
+    @Autowired
+    private final AuthorService service;
 
     @Override
-    @CommandHandler(value = "readAllAuthors")
-    public List<AuthorDtoResponse> readAll() {
-        return service.readAll();
+    @GetMapping
+    public ResponseEntity<List<AuthorDtoResponse>> readAllByPage
+            (@Min(1) @RequestParam int page,
+             @RequestParam(required = false, defaultValue = "5") int size,
+             @RequestParam(name = "sort_by", required = false, defaultValue = "name::asc") String sortBy) {
+        return new ResponseEntity<>(service.readAllPagedAndSorted(page, size, sortBy), HttpStatus.valueOf(200));
     }
 
     @Override
-    @CommandHandler(value = "readAuthor")
-    public AuthorDtoResponse readById(Long id) { return service.readById(id); }
-
-    @Override
-    @CommandHandler(value = "createAuthor")
-    public AuthorDtoResponse create(AuthorDtoRequest createRequest) { return service.create(createRequest); }
-
-    @Override
-    @CommandHandler(value = "updateAuthor")
-    public AuthorDtoResponse update(AuthorDtoRequest updateRequest) {
-        return service.update(updateRequest);
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDtoResponse> readById(@PathVariable Long id) {
+        return new ResponseEntity<>(service.readById(id), HttpStatus.valueOf(200));
     }
 
     @Override
-    @CommandHandler(value = "deleteAuthor")
-    public boolean deleteById(Long id) {
-        return service.deleteById(id);
+    @PostMapping("/create")
+    public ResponseEntity<AuthorDtoResponse> create(@RequestBody AuthorDtoRequest createRequest) {
+        return new ResponseEntity<>(service.create(createRequest), HttpStatus.valueOf(201));
     }
+
+    @Override
+    @PutMapping("/update/{id}")
+    public ResponseEntity<AuthorDtoResponse> update(@PathVariable Long id,
+                                                    @RequestBody AuthorDtoRequest updateRequest) {
+        return new ResponseEntity<>(service.update(updateRequest), HttpStatus.valueOf(200));
+    }
+
+    @Override
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+    }
+
+
 }
