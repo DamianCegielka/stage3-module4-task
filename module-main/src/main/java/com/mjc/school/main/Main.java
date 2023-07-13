592a3beaafd0ef5fafcab7bbf091c28ba643a6e1
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-13 16:13:51 +0200

    refactor main class, add jpaRepository

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -2,19 +2,18 @@ package com.mjc.school.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.io.IOException;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
+@EnableJpaRepositories("com.mjc.school.repository")
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ApplicationContext context = SpringApplication.run(Main.class, args);
-        Menu menu = context.getBean(Menu.class);
-        menu.mainController();
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+
     }
 }
