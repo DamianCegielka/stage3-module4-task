e49e2df6f86f224ed53ac8d0f12dac9c562ba6eb
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-13 16:20:14 +0200

    Small refactor AuthorServiceImpl

--- module-service/src/main/java/com/mjc/school/service/impl/AuthorServiceImpl.java
+++ module-service/src/main/java/com/mjc/school/service/impl/AuthorServiceImpl.java
@@ -1,7 +1,6 @@
 package com.mjc.school.service.impl;
 
-import com.mjc.school.repository.BaseRepository;
-import com.mjc.school.model.AuthorModel;
+import com.mjc.school.repository.AuthorRepository;
 import com.mjc.school.service.AuthorService;
 import com.mjc.school.service.Validator;
 import com.mjc.school.service.dto.author.AuthorDtoRequest;
@@ -9,64 +8,65 @@ import com.mjc.school.service.dto.author.AuthorDtoResponse;
 import com.mjc.school.service.mapper.AuthorDtoRequestMapperToAuthorModel;
 import com.mjc.school.service.mapper.AuthorModelMapperToAuthorDtoResponse;
 import com.mjc.school.service.mapper.ModelDtoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
 
-    @Autowired
-    BaseRepository<AuthorModel, Long> repository;
 
-    private AuthorModelMapperToAuthorDtoResponse mapAuthorModelToAuthorDtoResponse = new ModelDtoMapper.MapAuthorModelToAuthorDtoResponse();
-    private AuthorDtoRequestMapperToAuthorModel mapAuthorDtoRequestToAuthorModel = new ModelDtoMapper.MapAuthorDtoRequestToAuthorModel();
-    private Validator validator = new Validator();
+    private final AuthorRepository repository;
+    private final AuthorModelMapperToAuthorDtoResponse mapAuthorModelToAuthorDtoResponse = new ModelDtoMapper.MapAuthorModelToAuthorDtoResponse();
+    private final AuthorDtoRequestMapperToAuthorModel mapAuthorDtoRequestToAuthorModel = new ModelDtoMapper.MapAuthorDtoRequestToAuthorModel();
+    private final Validator validator = new Validator();
 
-    @Autowired
-    public AuthorServiceImpl(BaseRepository<AuthorModel, Long> repository) {
-        this.repository = repository;
-    }
 
     @Override
     public List<AuthorDtoResponse> readAll() {
-        List<AuthorDtoResponse> list = new ArrayList<>();
-        repository.readAll().forEach(x -> list.add(mapAuthorModelToAuthorDtoResponse.map(x)));
-        return list;
+        return repository.findAll().stream().map(mapAuthorModelToAuthorDtoResponse::map).toList();
     }
 
     @Override
     public AuthorDtoResponse readById(Long id) {
-        Optional<AuthorModel> authorDtoResponse = repository.readById(id);
+        /*Optional<AuthorModel> authorDtoResponse = repository.readById(id);
         if (authorDtoResponse.isPresent()) {
             return mapAuthorModelToAuthorDtoResponse.map(authorDtoResponse.get());
         }
+
+         */
         return null;
     }
 
     @Override
     public AuthorDtoResponse create(AuthorDtoRequest createRequest) {
-        try {
+        /*try {
             validator.lengthBetween3And15Symbols(createRequest.getName());
             AuthorModel authorModel = mapAuthorDtoRequestToAuthorModel.map(createRequest);
             return mapAuthorModelToAuthorDtoResponse.map(repository.create(authorModel));
         } catch (Exception e) {
         }
+
+         */
         return null;
     }
 
     @Override
     public AuthorDtoResponse update(AuthorDtoRequest updateRequest) {
+        /*
         AuthorModel authorModel = mapAuthorDtoRequestToAuthorModel.mapUpdate(updateRequest);
         return mapAuthorModelToAuthorDtoResponse.map(repository.update(authorModel));
+
+         */
+        return null;
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return repository.deleteById(id);
+        // return repository.deleteById(id);
+        return false;
     }
 
     @Override
