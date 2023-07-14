1cf02389fa840cfbf85af7cebb5ed4f2d97e5c47
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-14 13:49:17 +0200

    remove unused annotation and imports

--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -1,16 +1,15 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.annotations.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.news.NewsDtoRequest;
 import com.mjc.school.service.dto.news.NewsDtoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class NewsController implements BaseController<NewsDtoRequest, NewsDtoResponse, Long> {
 
     @Autowired
@@ -21,31 +20,26 @@ public class NewsController implements BaseController<NewsDtoRequest, NewsDtoRes
     }
 
     @Override
-    @CommandHandler(value = "createNews")
     public NewsDtoResponse create(NewsDtoRequest createRequest) {
         return service.create(createRequest);
     }
 
     @Override
-    @CommandHandler(value = "readAllNews")
     public List<NewsDtoResponse> readAll() {
         return service.readAll();
     }
 
     @Override
-    @CommandHandler(value = "readNews")
     public NewsDtoResponse readById(Long id) {
         return service.readById(id);
     }
 
     @Override
-    @CommandHandler(value = "updateNews")
     public NewsDtoResponse update(NewsDtoRequest updateRequest) {
         return service.update(updateRequest);
     }
 
     @Override
-    @CommandHandler(value = "deleteNews")
     public boolean deleteById(Long id) {
         return service.deleteById(id);
     }
--- module-web/src/main/java/com/mjc/school/controller/impl/TagController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/TagController.java
@@ -2,43 +2,37 @@ package com.mjc.school.controller.impl;
 
 
 import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.annotations.CommandHandler;
 import com.mjc.school.service.TagService;
 import com.mjc.school.service.dto.tag.TagDtoRequest;
 import com.mjc.school.service.dto.tag.TagDtoResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class TagController implements BaseController<TagDtoRequest, TagDtoResponse, Long> {
 
     public final TagService service;
 
     @Override
-    @CommandHandler(value = "createTag")
     public TagDtoResponse create(TagDtoRequest createRequest) { return service.create(createRequest);}
 
     @Override
-    @CommandHandler(value = "readAllTag")
     public List<TagDtoResponse> readAll() { return service.readAll(); }
 
     @Override
-    @CommandHandler(value = "readTag")
     public TagDtoResponse readById(Long id) {
         return service.readById(id);
     }
 
     @Override
-    @CommandHandler(value = "updateTag")
     public TagDtoResponse update(TagDtoRequest updateRequest) {
         return service.update(updateRequest);
     }
 
     @Override
-    @CommandHandler(value = "deleteTag")
     public boolean deleteById(Long id) {
         return service.deleteById(id);
     }
