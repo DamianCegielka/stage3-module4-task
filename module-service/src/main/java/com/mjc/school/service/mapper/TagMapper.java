381281a569caca59cb13a4c181997010268dffac
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-18 16:37:17 +0200

    small refactor TagMapper class

--- module-service/src/main/java/com/mjc/school/service/mapper/TagMapper.java
+++ module-service/src/main/java/com/mjc/school/service/mapper/TagMapper.java
@@ -5,15 +5,15 @@ import com.mjc.school.service.dto.tag.TagDtoRequest;
 import com.mjc.school.service.dto.tag.TagDtoResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE /*,uses = NewsMapper.class*/)
 public interface TagMapper {
 
     TagMapper INSTANCE = Mappers.getMapper(TagMapper.class);
 
     TagDtoResponse tagModelToDto(TagModel tagModel);
-
 
     @Mapping(target = "newsModels", ignore = true)
     TagModel tagDtoToModel(TagDtoRequest tagDtoRequest);
